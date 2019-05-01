e05b4be86acbaf20122292c324b837677b6f6b10
Bogdan Vaneev <warchantua@gmail.com>
2019-05-01 22:11:10 +0300

    Initial commit
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,12 @@
+# https://docs.hunter.sh/en/latest/packages/pkg/GTest.html
+hunter_add_package(GTest)
+find_package(GTest CONFIG REQUIRED)
+find_package(GMock CONFIG REQUIRED)
+
+# https://docs.hunter.sh/en/latest/packages/pkg/Boost.html
+hunter_add_package(Boost)
+find_package(Boost CONFIG REQUIRED)
+
+# https://docs.hunter.sh/en/latest/packages/pkg/libsodium.html
+hunter_add_package(libsodium)
+find_package(libsodium CONFIG REQUIRED)
